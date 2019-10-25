a9c635dfbd06aaae07587c62fe4fe8a98a6653cb
bcts369 <41810380+bcts369@users.noreply.github.com>
2019-10-25 10:23:08 +0800

    add interface and modules

--- src/x/identity/index.ts
+++ src/x/identity/index.ts
@@ -2,6 +2,8 @@ import { CosmosSDK } from "cosmos-client-ts";
 import { StdTx } from "cosmos-client-ts/lib/x/auth/types/std-tx";
 import { SetReq } from "./types/set-req";
 
+// refer identity rest.go tx.go
+
 export module Identity {
   export function get(sdk: CosmosSDK, address: string) {
     return sdk.get<{ [key: string]: string }>(`/identity/accounts/${address}`);
@@ -13,5 +15,22 @@ export module Identity {
     params: SetReq
   ) {
     return sdk.put<StdTx>(`/identity/accounts/${address}`, params);
+  }
+
+  export function post(
+    sdk: CosmosSDK,
+    address: string,
+    params: ImportReq
+  ) {
+    return sdk.post<StdTx>(`/identity/accounts/${address}/import`, params);
+  }
+
+  // delete演算子の `delete` とかぶってしまうので、関数名を `remove` に変更
+  export function remove(
+    sdk: CosmosSDK,
+    address: string,
+    params: DeleteReq
+  ) {
+    return sdk.delete<StdTx>(`/identity/accounts/${address}`, params);
   }
 }
