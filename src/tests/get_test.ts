e717abae26f8cdfd5596f9352ec52e3e528e6bb2
bcts369 <41810380+bcts369@users.noreply.github.com>
2019-11-13 17:21:19 +0800

    [WIP] add test
    BaseReqのメンバーの型がまだbitintのまま。ライブラリの更新待ち。

---
+++ src/tests/get_test.ts
@@ -0,0 +1,29 @@
+import { CosmosSDK } from 'cosmos-client-ts';
+import { Identity } from './../x/identity';
+
+
+const url: string = "http://133.130.77.80:1317";
+const chainId: string = "t";
+const sdk = new CosmosSDK(url, chainId);
+const address: string = "cosmos15g0309kcs0nfed829cwyc07s6ydpaalel6676h";
+
+async function getIdentity() {
+    try {
+        let result = await Identity.get(sdk, address)
+
+        console.log(result)
+    } catch (error) {
+        console.error("*** Error:", error);
+    }
+}
+
+getIdentity();
+
+
+// 次の記述でもOK（set、import、removeでも同様）
+
+Identity.get(sdk, address).then((item) => {
+    console.log("get結果", item);
+}).catch((e) => {
+    console.error("エラー内容:", e);
+});
---
+++ src/tests/import_test.ts
@@ -0,0 +1,56 @@
+import { CosmosSDK } from 'cosmos-client-ts';
+import { BaseReq } from 'cosmos-client-ts/lib/types/cosmos-sdk/rest'
+import { Coin } from 'cosmos-client-ts/lib/types/cosmos-sdk/coin'
+import { DecCoin } from 'cosmos-client-ts/lib/types/cosmos-sdk/deccoin'
+
+import { Identity } from './../x/identity';
+import { ImportReq } from '../x/identity/types/import-req';
+
+
+const url: string = "http://133.130.77.80:1317";
+const chainId: string = "t";
+const sdk = new CosmosSDK(url, chainId);
+const address: string = "cosmos15g0309kcs0nfed829cwyc07s6ydpaalel6676h";
+
+class MyCoin implements Coin {
+    denom = "stake";
+    amount = "200000";
+}
+
+class MyDecCoin implements DecCoin {
+    denom = "";
+    amount = "0";
+}
+
+let defaultValues = (): BaseReq => ({
+    from: address,
+    memo: "",
+    chain_id: chainId,
+    account_number: "0",
+    sequence: "0",
+    fees: [new MyCoin()],
+    gas_prices: [new MyDecCoin()],
+    gas: "0",
+    gas_adjustment: "",
+    simulate: false
+});
+
+async function importIdentity() {
+    let params: ImportReq = {
+        from_address: address,
+        base_req: defaultValues()
+    }
+    try {
+        let tx = await Identity.post(sdk, address, params)
+
+        console.log("結果:")
+        console.log("fee", tx.fee);
+        console.log("msg", tx.msg);
+        console.log("signature", tx.signatures);
+        console.log("memo", tx.memo);
+    } catch (error) {
+        console.error("*** Error:", error);
+    }
+}
+
+importIdentity();
